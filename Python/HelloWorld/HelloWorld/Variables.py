6effaa3439f5dfae15722da3aead66fd4f2a16a2
Baiju Mathew <baiju.mathew@autodesk.com>
2015-07-17 16:05:55 +0800

    Module -3 Variables

---
+++ Python/HelloWorld/HelloWorld/Variables.py
@@ -0,0 +1,23 @@
+# prompt the user for a value and print it on the screen
+
+# Variables are easy to be defined. No delaration :) Dangerous too :((
+
+name = input("What's your name -- > ")
+print(name)
+
+# change the value
+print ("\nValue changed")
+name = "Ashley Mathew"
+print(name)
+
+#define a number
+print ("\Hardcode a number and add it by 1")
+number = 9
+print(number + 1) # very easy to perform mathematical operations
+
+# the name of the tool he has used to create presentation
+# ZoomIt
+# Pretty good tool
+
+# Python variables are case sensitive, cannot start with a number
+# They can't have spaces
