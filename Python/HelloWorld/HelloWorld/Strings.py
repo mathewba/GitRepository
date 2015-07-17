80abed4f5640728d12bc804c0bd2373fc4d74e24
Baiju Mathew <baiju.mathew@autodesk.com>
2015-07-17 15:43:01 +0800

    Module 2 - Displaying strings

---
+++ Python/HelloWorld/HelloWorld/Strings.py
@@ -0,0 +1,5 @@
+print('Hickory Dickory Dock')
+
+print("Dinosours are biggggggg\nI am a fan of Jurassic World.")
+
+print("It's pertty amazing to watch this movie in the big screen \nI am gonna miss it.")
