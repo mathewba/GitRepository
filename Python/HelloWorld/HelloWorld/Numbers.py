0ec7552a0da0437c2a7bdb9a47a390ab42a768f6
Baiju Mathew <baiju.mathew@autodesk.com>
2015-07-17 17:30:40 +0800

    Module 4 Numbers

---
+++ Python/HelloWorld/HelloWorld/Numbers.py
@@ -0,0 +1,12 @@
+# Area of a rectangle
+length = 50
+breadth = 2
+
+area = length * breadth
+
+print ("Area of the reactangle is %d" % area) # Throws error as area is a number. + is only for strings.
+
+# %d,%f is used for numbers and float. More like C++.
+
+print ("My fav number is %6d" %2) # Allignment
+
