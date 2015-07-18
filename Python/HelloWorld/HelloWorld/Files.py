5755012bebe180e46553afd9707a50be1c331ed4
Baiju Mathew <baiju.mathew@autodesk.com>
2015-07-18 23:55:22 +0800

    Module 8 File Operations

---
+++ Python/HelloWorld/HelloWorld/Files.py
@@ -0,0 +1,12 @@
+# Sample code to open a file and read the text and print it
+
+# File is C:\Users\Home\Documents\Visual Studio 2015\Backup Files\HelloWorld\FileSample.xml
+
+import sys
+
+from sys import argv
+
+txt = open("C:\\Users\\Home\\Documents\\Visual Studio 2015\\Backup Files\\HelloWorld\\FileSample.xml")
+
+print (txt.readlines())
+
