dfa1ec471b34c2b1ca3e66f9ebbf9b838d0fe19b
Baiju Mathew <baiju.mathew@autodesk.com>
2015-08-16 10:14:05 +0800

    Adding a batch file to test the logging workflow

--- Python/HelloWorld/InstallLogger/InstallLogger.py
+++ Python/HelloWorld/InstallLogger/InstallLogger.py
@@ -1,30 +1,40 @@
 ﻿import sys, logging
 
+from sys import argv
+
 # include the logging module
 
 from rainbow_logging_handler import RainbowLoggingHandler
 
 def main_func():
-    # setup `logging` module
-    logger = logging.getLogger('test_logging')
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("[%(asctime)s] %(name)s %(funcName)s():%(lineno)d\t%(message)s")  # same as default
 
-    # setup `RainbowLoggingHandler`
-    handler = RainbowLoggingHandler(sys.stderr, color_funcName=('black', 'yellow', True))
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    # Check the number of inputs
+    numofinputs = len(sys.argv)
 
-    logger.debug("debug msg")
-    logger.info("info msg")
-    logger.warn("warn msg")
-    logger.error("error msg")
-    logger.critical("critical msg")
+    # Conditional expressions
+    if numofinputs > 2 : 
+        print ("Invalid arguments \nGood BYE")
+    else :
+        # setup `logging` module
+        logger = logging.getLogger('test_logging')
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter("[%(asctime)s] %(name)s %(funcName)s():%(lineno)d\t%(message)s")  # same as default
 
-    try:
-        raise RuntimeError("Opa!")
-    except Exception as e:
-        logger.exception(e)
+        # setup `RainbowLoggingHandler`
+        handler = RainbowLoggingHandler(sys.stderr, color_funcName=('black', 'black', True))
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        logger.debug("debug msg")
+        logger.info("info msg")
+        logger.warn("warn msg")
+        logger.error("error msg")
+        logger.critical("critical msg")
+
+        try:
+            raise RuntimeError("Aiyo!")
+        except Exception as e:
+            logger.exception(e)
 
 if __name__ == '__main__':
     main_func()
